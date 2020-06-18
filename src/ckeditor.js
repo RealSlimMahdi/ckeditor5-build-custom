3e89ab2b830847d0d22c5901b981e3a9f87c0606
RealSlimMahdi <RealSlimMahdi@users.noreply.github.com>
2020-06-18 13:32:13 +0200

    last

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -66,40 +66,40 @@ Editor.builtinPlugins = [
 
 export default Editor;
 
-// Editor configuration.
-ClassicEditor.defaultConfig = {
-	toolbar: {
-		items: [
-			'heading',
-			'|',
-			'bold',
-			'italic',
-			'link',
-			'bulletedList',
-			'numberedList',
-			'|',
-			'indent',
-			'outdent',
-			'|',
-			'imageUpload',
-			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
-			'undo',
-			'redo'
-		]
-	},
-	image: {
-		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
-			'|',
-			'imageTextAlternative'
-		]
-	},
-	table: {
-		contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ]
-	},
-	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
-};
+// // Editor configuration.
+// ClassicEditor.defaultConfig = {
+// 	toolbar: {
+// 		items: [
+// 			'heading',
+// 			'|',
+// 			'bold',
+// 			'italic',
+// 			'link',
+// 			'bulletedList',
+// 			'numberedList',
+// 			'|',
+// 			'indent',
+// 			'outdent',
+// 			'|',
+// 			'imageUpload',
+// 			'blockQuote',
+// 			'insertTable',
+// 			'mediaEmbed',
+// 			'undo',
+// 			'redo'
+// 		]
+// 	},
+// 	image: {
+// 		toolbar: [
+// 			'imageStyle:full',
+// 			'imageStyle:side',
+// 			'|',
+// 			'imageTextAlternative'
+// 		]
+// 	},
+// 	table: {
+// 		contentToolbar: [ 'tableColumn', 'tableRow', 'mergeTableCells' ]
+// 	},
+// 	// This value must be kept in sync with the language defined in webpack.config.js.
+// 	language: 'en'
+// };
